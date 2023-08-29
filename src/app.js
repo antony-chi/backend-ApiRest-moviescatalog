c9103fff0d14e4ba7e65c10ea7456a50640a0e4d
antony-chi <antony.cp9@gmail.com>
2023-08-29 01:17:01 -0600

    se agrego cors module

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 const app = express();
 import morgan from "morgan";
+import cors from "cors"
 import cookieParser from "cookie-parser";
 
 //---importamos routes -----
@@ -8,7 +9,9 @@ import MoviesRoouter from "./routes/peliculas.router.js";
 import userRouter from "./routes/user.router.js";
 
 //--global setting---
-app.use(morgan("dev")); //    use morgan para ver las peticiones en consola
+app.use(morgan("dev"));
+app.use(cors())
+//    use morgan para ver las peticiones en consola
 app.use(express.urlencoded({ extended: false })); //para usar urlencode que enviamos desd api client (portman)
 app.use(express.json()); //indicamos que use json que envian api client
 app.use(cookieParser()); //para usar cookie de los headers
